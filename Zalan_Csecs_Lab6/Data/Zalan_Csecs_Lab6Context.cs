1c4f444817d23a3a1261b89d4b522a32ae44854f
ZALÁN CSÉCS <zalan.csecs@stud.ubbcluj.ro>
2021-01-09 08:58:20 +0200

    Add project files.

---
+++ Zalan_Csecs_Lab6/Data/Zalan_Csecs_Lab6Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Zalan_Csecs_Lab6.Models;
+
+namespace Zalan_Csecs_Lab6.Data
+{
+    public class Zalan_Csecs_Lab6Context : DbContext
+    {
+        public Zalan_Csecs_Lab6Context (DbContextOptions<Zalan_Csecs_Lab6Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Zalan_Csecs_Lab6.Models.Book> Book { get; set; }
+    }
+}
---
+++ Zalan_Csecs_Lab6/Pages/Books/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Zalan_Csecs_Lab6.Data;
+using Zalan_Csecs_Lab6.Models;
+
+namespace Zalan_Csecs_Lab6.Pages.Books
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Zalan_Csecs_Lab6.Data.Zalan_Csecs_Lab6Context _context;
+
+        public IndexModel(Zalan_Csecs_Lab6.Data.Zalan_Csecs_Lab6Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Book> Book { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Book = await _context.Book.ToListAsync();
+        }
+    }
+}
